de4649389770eb5da02597d9cbee32393f30f52e
Timothy Scott <timjimski@gmail.com>
2017-02-06 09:17:17 -0500

    Added code to check validity on realorsatire.com

--- WebScraper/ScrapyProject/ScrapyProject/spiders/news.py
+++ WebScraper/ScrapyProject/ScrapyProject/spiders/news.py
@@ -5,6 +5,9 @@ from urllib.parse import urlparse
 
 visited = set()
 
+realsites = set()
+fakesites = set()
+
 
 class NewsSpider(scrapy.Spider):
     name = "news"
@@ -14,14 +17,27 @@ class NewsSpider(scrapy.Spider):
 
     # This function parses realorsatire.com to find the news sites to parse
     def parse(self, response):
-        links = response.css("h2 > a::text").extract() # Find all links to outside news sites on this page
-        for link in links:
-            print("Starting to scrape " + link)
-            # TODO: Determine if url is real or satire, and find a way to pass that info along to the callback
-            yield scrapy.Request(url="http://" + link, callback=self.parsenewssite)
+        # links = response.css("h2 > a::text").extract()  # Find all links to outside news sites on this page
+        articles = response.css("article")  # This site is very nicely formatted. Every article is in <article> tags
+        for article in articles:
+            link = article.css("h2.entry-title > a::text").extract()[0]
+            # link = "http://" + link
+            print("Visiting link " + link)
+            valid = article.css("footer > h3 > a::text").extract()
+            print("VALIDITY: " + valid[0])
+            if len(valid) != 1 or not (valid[0].lower() == "real" or valid[0].lower() == "satire"):
+                print("Never mind, " + link + " is hard to pin down.")
+            else:
+                valid = valid[0].lower() == "real"
+                if valid:
+                    realsites.add(link)
+                else:
+                    fakesites.add(link)
+                yield scrapy.Request(url="http://" + link, callback=self.parsenewssite)
 
-        match = re.match(r'^.*/page/([0-9]+)/$', response.url).group(1) # Find the page number in the URL
-        if match is None: # No match is found on the first page, because the page number isn't in the URL
+        # The following code all just goes to the next page of realorsatire.com
+        match = re.match(r'^.*/page/([0-9]+)/$', response.url)  # Find the page number in the URL
+        if match is None:  # No match is found on the first page, because the page number isn't in the URL
             pagenum = 1
         else:
             pagenum = int(match.group(1))
@@ -35,14 +51,15 @@ class NewsSpider(scrapy.Spider):
     # This function parses the pages of the actual news site
     def parsenewssite(self, response):
         uri = urlparse(response.url)
-        domain = uri.netloc # The domain name of the current page
+        domain = uri.netloc  # The domain name of the current page
 
         # If this URL looks like an article...
         if re.match(r'.*?((?:[a-z0-9_]+)(?:-[a-z0-9_]+)+)/?$', uri.path, flags=re.IGNORECASE):
             # TODO: Find contents of article (inside <p> tags), determine if it is long enough to be considered an
             #  actual article, then yield it. (Along with other data, like the domain and whatnot)
             yield {
-                'url': response.url
+                'url': response.url,
+                # 'valid': domain in realsites
             }
 
         # Find all links on the page that go to the same URL that we're currently on
