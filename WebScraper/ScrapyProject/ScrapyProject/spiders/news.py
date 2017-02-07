131f5b4da5b8e3066979515966175cae592a5f0b
Timothy Scott <timjimski@gmail.com>
2017-02-06 21:39:50 -0500

    All visited websites are now stored in the database

--- WebScraper/ScrapyProject/ScrapyProject/spiders/news.py
+++ WebScraper/ScrapyProject/ScrapyProject/spiders/news.py
@@ -1,84 +1,107 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import os
 from urllib.parse import urlparse
+import json
+import psycopg2
+from psycopg2 import pool
 
-visited = set()
 
-realsites = set()
-fakesites = set()
+dir = os.path.dirname(__file__)
+configpath = os.path.join(dir, "../../../dbsettings.json")
+with open(configpath) as configFile:
+    config = json.load(configFile)
+
+connectionPool = pool.ThreadedConnectionPool(5, 50, database=config["database"], host=config["host"],
+                                             user=config["user"], password=config["password"], port=config["port"])
 
 
 class NewsSpider(scrapy.Spider):
+
     name = "news"
-    # allowed_domains = ["huffingtonpost.com"]
-    start_urls = ['http://realorsatire.com/websites-that-are/real/',
-                  'http://realorsatire.com/websites-that-are/satire/']
 
-    # This function parses realorsatire.com to find the news sites to parse
+    def removewww(self, url):
+        return re.sub(r'^((?:https?://)?)(w[w0-9][w0-9]\.)?(.*?)$', r'\1\3', url, flags=re.IGNORECASE)
+
+    def start_requests(self):
+        with ConnectionWrapper(connectionPool) as connection:
+            with connection.cursor() as cursor:
+                cursor = connection.cursor()
+                cursor.execute("SELECT url, valid FROM sources;")
+
+                for url in cursor.fetchmany(10000):
+                    yield scrapy.Request(url="http://" + url[0], callback=self.parse,
+                                         meta={'domain': url[0], 'valid': url[1].lower() == 'true'})
+
     def parse(self, response):
-        # links = response.css("h2 > a::text").extract()  # Find all links to outside news sites on this page
-        articles = response.css("article")  # This site is very nicely formatted. Every article is in <article> tags
-        for article in articles:
-            link = article.css("h2.entry-title > a::text").extract()[0]
-            # link = "http://" + link
-            print("Visiting link " + link)
-            valid = article.css("footer > h3 > a::text").extract()
-            print("VALIDITY: " + valid[0])
-            if len(valid) != 1 or not (valid[0].lower() == "real" or valid[0].lower() == "satire"):
-                print("Never mind, " + link + " is hard to pin down.")
-            else:
-                valid = valid[0].lower() == "real"
 
-                # TODO: Use database instead of in-memory sets
-                if valid:
-                    print("Added " + link + " to realsites")
-                    realsites.add(link.replace("www.", ""))
-                else:
-                    print("Added " + link + " to fakesites")
-                    fakesites.add(link.replace("www.", ""))
-                yield scrapy.Request(url="http://" + link, callback=self.parsenewssite)
+        currenturi = urlparse(response.url)
+        currentdomain = currenturi.netloc  # The domain name of the current page
 
-        # The following code all just goes to the next page of realorsatire.com
-        match = re.match(r'^.*/page/([0-9]+)/$', response.url)  # Find the page number in the URL
-        if match is None:  # No match is found on the first page, because the page number isn't in the URL
-            pagenum = 1
-        else:
-            pagenum = int(match.group(1))
-        pagenum += 1
+        cleanedurl = "{uri.scheme}://{uri.netloc}{uri.path}".format(uri=currenturi)
 
-        # This regex removes the page number from the url, if it's there, or just doesn't change anything otherwise
-        nextpageurl = re.match(r'(^.*?)(/page/[0-9]+/)?$', response.url).group(1) + "/page/" + str(pagenum) + "/"
-        print("Moving on to page " + nextpageurl)
-        yield scrapy.Request(url=nextpageurl, callback=self.parse)
+        if response.meta.get("domain") != self.removewww(currentdomain):
+            print("Domains changed. Expected: " + response.meta.get("domain") + ", got: " + response.url)
+            return  # This happens when we were redirected (probably because of an ad)
 
-    # This function parses the pages of the actual news site
-    def parsenewssite(self, response):
-        uri = urlparse(response.url)
-        domain = uri.netloc  # The domain name of the current page
+        with ConnectionWrapper(connectionPool) as connection:
+            with connection.cursor() as cursor:
 
-        # If this URL looks like an article...
-        if re.match(r'.*?((?:[a-z0-9_]+)(?:-[a-z0-9_]+)+)/?$', uri.path, flags=re.IGNORECASE):
-            # TODO: Find contents of article (inside <p> tags), determine if it is long enough to be considered an
-            #  actual article, then yield it. (Along with other data, like the domain and whatnot)
-            valid = (domain.replace("www.", "") in realsites)
-            yield {
-                'url': response.url,
-                'valid': valid
-            }
+                cursor.execute("INSERT INTO visited (url, domain) VALUES (%s, %s)",
+                               (cleanedurl, self.removewww(currentdomain)))
 
-        # Find all links on the page that go to the same URL that we're currently on
-        # (We don't want to follow links to ads or other sites or whatever)
-        links = response.css("a[href*='" + uri.netloc + "']::attr(href)").extract()
-        for link in links:
-            # print("About to follow link: " + link)
-            newuri = urlparse(link)
-            # Specifically remove anything in the url that's a parameter or something like that, for reasons
-            # (Many links have a bunch of parameters used by the site for tracking, so it makes it difficult
-            # to keep track of which URLs have already been visited. So we remove all the parameters)
-            url = "{uri.scheme}://{uri.netloc}{uri.path}".format(uri=newuri)
+                # If this URL looks like an article...
+                if re.match(r'.*?((?:[a-z0-9_]+)(?:-[a-z0-9_]+)*)/?$', currenturi.path, flags=re.IGNORECASE):
+                    # TODO: Find contents of article (inside <p> tags), determine if it is long enough to be considered
+                    # an actual article, then yield it. (Along with other data, like the domain and whatnot)
+                    yield {
+                        'url': response.url,
+                        'valid': response.meta.get("valid")
+                    }
 
-            # TODO: Maybe store the visited URLs in the database instead of in memory?
-            if url not in visited and domain == newuri.netloc:
-                visited.add(url)
-                yield scrapy.Request(url=url, callback=self.parsenewssite)
+                connection.commit()
+
+                # Find all links on the page that go to the same URL that we're currently on
+                # (We don't want to follow links to ads or other sites or whatever)
+                links = response.css("a::attr(href)").extract()
+                for link in links:
+                    # print("About to follow link: " + link)
+                    newuri = urlparse(link)
+                    # Specifically remove anything in the url that's a parameter or something like that, for reasons
+                    # (Many links have a bunch of parameters used by the site for tracking, so it makes it difficult
+                    # to keep track of which URLs have already been visited. So we remove all the parameters)
+                    url = "{uri.scheme}://{uri.netloc}{uri.path}".format(uri=newuri)
+
+                    # TODO: Maybe store the visited URLs in the database instead of in memory?
+                    cursor.execute("SELECT count(1) FROM visited WHERE url=%s", (self.removewww(newuri.netloc), ))
+                    alreadyvisited = cursor.fetchone()[0] == '1'
+                    if alreadyvisited and currentdomain == newuri.netloc:
+                        yield scrapy.Request(url=url, callback=self.parse, meta=response.meta)
+
+
+class ConnectionWrapper(object):
+
+    def __init__(self, connpool):
+        # self.key = random.randint(1, 1000000)
+        self.connpool = connpool
+
+    def __enter__(self):
+        self.connection = self.connpool.getconn()
+        print("Got connection ")
+        return self.connection
+
+    def __exit__(self, *args):
+        # if self.connection.status == psycopg2.extensions.STATUS_IN_TRANSACTION:
+        # print("Rolling back connection")
+
+        try:
+            self.connpool.putconn(self.connection)
+            print("Released connection")
+        except pool.PoolError:
+            pass
+
+        # print("Put connection ")
+
+    def getconnection(self):
+        return self.connection
