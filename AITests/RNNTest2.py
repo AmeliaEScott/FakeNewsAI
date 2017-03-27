70e79b4b3f41763d2287a67c2711fad0a208ab34
Timothy Scott <timjimski@gmail.com>
2017-03-27 14:45:39 -0400

    It at least runs with 0 GPUs. Testing on server to follow

--- AITests/RNNTest2.py
+++ AITests/RNNTest2.py
@@ -38,7 +38,7 @@ VARIABLE_SAVE_FILE = "VariableCheckpoints/FakeNewsAIVariables.ckpt"
 # Batch size of 1 means each article is its own batch
 # For the sake of me not having to bugfix, this number
 # should be evenly divisible by NUM_GPUS if NUM_GPUS > 0
-BATCH_SIZE = 100
+BATCH_SIZE = 10
 
 # Size of a vector for an individual word
 WORD_VECTOR_SIZE = 300
@@ -63,7 +63,7 @@ VARIABLE_SCOPE = "fakenewsvariablescope"
 LEARNING_RATE = 1.0
 
 
-def buildtower(batchsize):
+def buildtower(batchsize, networkinput, initial_state, initial_hidden_state, expected_outputs, loss_mask):
     """
     Builds a single instance of a tower.
     A tower, in this case, is the entire neural network up to the point where it calculates the gradient.
@@ -80,13 +80,13 @@ def buildtower(batchsize):
     # Except where otherwise stated, this code all does roughly the same thing here.
 
     # The inputs to the neural network
-    networkinput = tf.placeholder(tf.float32, shape=[BATCH_SIZE, None, WORD_VECTOR_SIZE * WORDS_INPUT_AT_ONCE],
-                                  name="inputPlaceholder")
+    # networkinput = tf.placeholder(tf.float32, shape=[BATCH_SIZE, None, WORD_VECTOR_SIZE * WORDS_INPUT_AT_ONCE],
+    #                               name="inputPlaceholder")
 
     # States for the LSTM
-    initial_state = tf.placeholder(tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name="initialStatePlaceholder")
-    initial_hidden_state = tf.placeholder(tf.float32, shape=[BATCH_SIZE, STATE_SIZE],
-                                          name="initialHiddenStatePlaceholder")
+    # initial_state = tf.placeholder(tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name="initialStatePlaceholder")
+    # initial_hidden_state = tf.placeholder(tf.float32, shape=[BATCH_SIZE, STATE_SIZE],
+    #                                       name="initialHiddenStatePlaceholder")
     initial_state_tuple = tf.contrib.rnn.LSTMStateTuple(initial_state, initial_hidden_state)
 
 
@@ -104,10 +104,10 @@ def buildtower(batchsize):
     # Build the output layers
     rnn_outputs_reshaped = tf.reshape(rnn_outputs, [-1, STATE_SIZE])
     network_outputs = tf.sigmoid(tf.matmul(rnn_outputs_reshaped, weights) + biases)
-    expected_outputs = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="expectedOutputsPlaceholder")
+    # expected_outputs = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="expectedOutputsPlaceholder")
     expected_outputs_reshaped = tf.reshape(expected_outputs, [-1, 1])
 
-    loss_mask = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="lossMaskPlaceholder")
+    # loss_mask = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="lossMaskPlaceholder")
     loss_mask_reshaped = tf.reshape(loss_mask, shape=[-1])
 
     expected_outputs_reshaped = loss_mask_reshaped * expected_outputs_reshaped
@@ -115,7 +115,7 @@ def buildtower(batchsize):
 
     loss = tf.losses.mean_squared_error(labels=expected_outputs_reshaped, predictions=network_outputs)
 
-    return networkinput, initial_state, initial_hidden_state, loss, loss_mask, network_outputs
+    return loss
 
 
 # This code is courtesy of the Tensorflow open source examples.
@@ -163,13 +163,8 @@ def buildgraph():
     :return:
     """
 
-    networkinputs = []
-    initial_states = []
-    initial_hidden_states = []
     losses = []
-    lossmasks = []
     gradients = []
-    networkoutputs = []
 
     # This list is the list of each device for which we should make a tower. If there are no GPUs, then there
     # should be exactly 1 tower, on the CPU. If there are 1 or more GPUs, then there should be a tower on
@@ -178,24 +173,35 @@ def buildgraph():
 
     optimizer = tf.train.AdagradOptimizer(LEARNING_RATE)
 
-    with tf.variable_scope(VARIABLE_SCOPE), tf.device("/cpu:0"):
-        weights = tf.get_variable(WEIGHTS_NAME, shape=[STATE_SIZE, 1], dtype=tf.float32,
-                                  initializer=tf.random_uniform_initializer(minval=-1, maxval=1, dtype=tf.float32))
-        biases = tf.get_variable(BIASES_NAME, shape=[1], dtype=tf.float32,
-                                 initializer=tf.constant_initializer(0.0, dtype=tf.float32))
+    with tf.device("/cpu:0"):
+        with tf.variable_scope(VARIABLE_SCOPE):
+            weights = tf.get_variable(WEIGHTS_NAME, shape=[STATE_SIZE, 1], dtype=tf.float32,
+                                      initializer=tf.random_uniform_initializer(minval=-1, maxval=1, dtype=tf.float32))
+            biases = tf.get_variable(BIASES_NAME, shape=[1], dtype=tf.float32,
+                                     initializer=tf.constant_initializer(0.0, dtype=tf.float32))
+        network_input = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, WORD_VECTOR_SIZE], name="Inputs")
+        initial_state = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name="InitialState")
+        initial_hidden_state = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, STATE_SIZE], name="InitialHiddenState")
+        expected_output = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="ExpectedOutputs")
+        loss_mask = tf.placeholder(dtype=tf.float32, shape=[BATCH_SIZE, None, 1], name="lossMaskPlaceholder")
 
-    for gpu in gpus:
+        network_input_split = tf.split(network_input, len(gpus), axis=0)
+        initial_state_split = tf.split(initial_state, len(gpus), axis=0)
+        initial_hidden_state_split = tf.split(initial_hidden_state, len(gpus), axis=0)
+        expected_output_split = tf.split(expected_output, len(gpus), axis=0)
+        loss_mask_split = tf.split(loss_mask, len(gpus), axis=0)
+
+    for i in range(0, len(gpus)):
+        gpu = gpus[i]
         # This line assures that all following code will be run in the specified device. For a more concrete
         # example of this, see the similar code a few lines down
         with tf.device(gpu):
-            networkinput, initial_state, initial_hidden_state, loss, lossmask, networkoutputs_ = buildtower(BATCH_SIZE / len(gpus))
-            networkinputs.append(networkinput)
-            initial_states.append(initial_state)
-            initial_hidden_states.append(initial_hidden_state)
+            loss = buildtower(BATCH_SIZE / len(gpus), networkinput=network_input_split[i],
+                              initial_state=initial_state_split[i],
+                              initial_hidden_state=initial_hidden_state_split[i],
+                              expected_outputs=expected_output_split[i], loss_mask=loss_mask_split[i])
             losses.append(loss)
-            lossmasks.append(lossmask)
             gradients.append(optimizer.compute_gradients(loss))
-            networkoutputs.append(networkoutputs_)
 
     # This assures that all the following code will run in the CPU, and not the GPU.
     # More specifically, any graph operation constructed in this "with" block will be run in the CPU
@@ -210,15 +216,10 @@ def buildgraph():
 
         # The inputs, hidden states, etc are currently split up between multiple towers.
         # Here, we just stick them all together so we can input them as we did before.
-        allnetworkinputs = tf.concat(networkinputs, axis=0)
-        allinitialstates = tf.concat(initial_states, axis=0)
-        allinitialhiddenstates = tf.concat(initial_hidden_states, axis=0)
         averagelosses = sum(losses) / len(losses)
-        alllossmasks = tf.concat(lossmasks, axis=0)
-        alloutputs = tf.concat(networkoutputs, axis=0)
 
-    return allnetworkinputs, alloutputs, allinitialstates, allinitialhiddenstates, \
-        averagelosses, train_step, alllossmasks
+    return network_input, expected_output, initial_state, initial_hidden_state, \
+        averagelosses, train_step, loss_mask
 
 
 def getpaddedbatches(model):
