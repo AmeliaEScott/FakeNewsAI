83c31dc15c5a07e9097e997716bd71b1603a50a7
Timothy Scott <timjimski@gmail.com>
2017-04-05 12:27:05 -0400

    Added some comments and stub functions

---
+++ FakeNewsWebsite/FakeNewsApp/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from . import views
+
+"""
+This fils is responsible for mapping URLs to Views.
+A View is a python function that is executed when someone requests a specific URL, which returns
+some sort of data. It could be HTML, or JSON, or XML, or whatever else.
+
+For each url in the list below, the first argument is a regex for matching URLs. Django automatically cuts
+off the domain of the url, so you're just left with what's after the first slash. For example,
+if you visit http://website.com/something/url, Django gives you 'something/url'.
+
+The second argument is the function to be executed when this URL is requested. For example, the following:
+url(r'^', views.testview)
+Maps the URL 'http://website.com/' to the function testview in the python file views.py.
+"""
+
+urlpatterns = [
+    url(r'^', views.testview)
+]
--- FakeNewsWebsite/FakeNewsApp/views.py
+++ FakeNewsWebsite/FakeNewsApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+"""
+In Django, a 'view' is a python function that is given the HTTP request that was sent to the server,
+and returns some sort of response. This could be, for example, some HTML, or some JSON, or a 404 error.
+
+See FakeNewsApp/urls.py for how to connect a specific URL to a specific view.
+"""
+
+
+def testview(request):
+    return HttpResponse("This is not a very good website.")
